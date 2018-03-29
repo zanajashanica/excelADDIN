4b505468b4965a4867c9cc9446777dc48e090af6
zana jashanica <zana.j@live.com>
2018-03-29 12:32:28 +0200

    first commit

---
+++ ExcelAddIn1/Ribbon1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+
+namespace ExcelAddIn1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, RibbonControlEventArgs e)
+        {
+            //open excel worksheet
+            Excel.Worksheet activeWorkSheet = Globals.ThisAddIn.Application.ActiveSheet;
+            //get cell
+            Excel.Range actCell = Globals.ThisAddIn.Application.ActiveCell;
+          //  Type type = activeWorkSheet.Cells.Value.GetType();
+            if(actCell.Value != null )
+            {
+                Type type = actCell.Value.GetType();
+                MessageBox.Show("The type data of the cell is " + type);
+            }
+        }
+    }
+}
